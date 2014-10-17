a6963eaca580f80200082617acbc74583855394b
nikita <robotomize@gmail.com>
2014-10-17 13:35:41 +1100

    update #6

--- index.php
+++ index.php
@@ -360,9 +360,9 @@ for($mm=0;$mm<count($GroupIds['0']);$mm++)
     if($strr == "") $strr = $GroupIds['0'][$mm];
     else $strr = $strr.",".$GroupIds['0'][$mm];
 }
-echo $strr;
+//echo $strr;
 $Groupinfo[] = $vk->getGroupsById($strr);
-var_dump($Groupinfo);
+
 
     $codeStr = 'var a=API.groups.get({"user_id":"'.$_GET['id'].'"}); var b=a; var d=0;
         var c = [];
@@ -374,12 +374,30 @@ var_dump($Groupinfo);
         return c;';       
 
 $viewMyFeed[] = $vk->getExecuteFeedFriends($codeStr);
+
+
+     $end_time = microtime();
+    $end_array = explode(" ",$end_time);
+    $end_time = $end_array[1] + $end_array[0];
+    $time = $end_time - $start_time;
+    printf("Страница сгенерирована за %f секунд",$time)."<br>";
+var_dump($Groupinfo['0']);
     for($cc=1; $cc<count($viewMyFeed['0']); $cc++)
     {
         
         for($jj = 1; $jj<4; $jj++)
         {    
               // echo "<img src=".$viewMyFeed['0'][$cc]['groups']['0']['photo'].">&nbsp".$viewMyFeed['0'][$cc]['groups']['0']['name']."\n<br><br>"; 
+               //echo $viewMyFeed['0'][$cc][$jj]['from_id']."\n<br>";
+               for($vv=0; $vv<count($Groupinfo['0']);$vv++)
+               {
+
+                    if("-".$Groupinfo['0'][$vv]['gid'] == $viewMyFeed['0'][$cc][$jj]['from_id']) 
+                    {
+                        echo "<img src=".$Groupinfo['0'][$vv]['photo']."> ".$Groupinfo['0'][$vv]['name']."\n<br>";
+                        break;
+                    }
+               }
                echo $viewMyFeed['0'][$cc][$jj]['text']."\n<br>";
                echo "<img src=".$viewMyFeed['0'][$cc][$jj]['attachments']['0']['photo']['src_big'].">\n<br>";
                echo "\n<br><br><br>";
@@ -387,6 +405,7 @@ $viewMyFeed[] = $vk->getExecuteFeedFriends($codeStr);
     
 
     }
+    var_dump($viewMyFeed['0']);
      
      // echo $cc;
           
@@ -396,11 +415,6 @@ $viewMyFeed[] = $vk->getExecuteFeedFriends($codeStr);
    //var_dump($viewUsrWallcache); 
           
 
-     $end_time = microtime();
-    $end_array = explode(" ",$end_time);
-    $end_time = $end_array[1] + $end_array[0];
-    $time = $end_time - $start_time;
-    printf("Страница сгенерирована за %f секунд",$time)."<br>";
       //var_dump($viewMyFeed);
     
 }
